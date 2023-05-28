b28df7ccd4ddcc6d081804f223b6361a5c9151ef
antil-samarth <64965361+antil-samarth@users.noreply.github.com>
2023-05-28 18:45:18 +0530

    1

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from dotenv import load_dotenv
 import os
 
 app = Flask(__name__)
+load_dotenv()
 
 mydb = mysql.connector.connect(
-	host=os.getenv('MYSQLHOST'),
-	port=os.getenv('MYSQLPORT'),
-	database=os.getenv('MYSQLDATABASE'),
- 	user=os.getenv('MYSQLUSER'),
- 	password=os.getenv('MYSQLPASSWORD')
+    host=os.getenv('MYSQLHOST'),
+    port=os.getenv('MYSQLPORT'),
+    database=os.getenv('MYSQLDATABASE'),
+    user=os.getenv('MYSQLUSER'),
+    password=os.getenv('MYSQLPASSWORD')
 )
 
 @app.route('/test', methods=['GET'])
@@ -24,13 +25,11 @@ def get_test():
 @app.route('/admin', methods=['POST'])
 def update_test():
     new_word = request.form['new_word']
-    cur = mysql.connection.cursor()
+    cur = mydb.cursor()
     cur.execute("UPDATE test_table SET test_word = %s", (new_word,))
-    mysql.connection.commit()
+    mydb.commit()
     cur.close()
     return 'Word updated successfully'
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
